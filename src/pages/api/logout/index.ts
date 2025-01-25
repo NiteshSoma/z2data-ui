b8456fb4bad6b664f3342b2c018ed727042df1fb
soma nitesh <soma.nitesh18@gmail.com>
2025-01-25 02:37:45 +0000

    session management apis comments updated

--- src/pages/api/logout/index.ts
+++ src/pages/api/logout/index.ts
@@ -1,17 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import cookie from 'cookie';
+import { NextApiRequest, NextApiResponse } from "next";
+import cookie from "cookie";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        res.setHeader('Set-Cookie', cookie.serialize('userSession', '', {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            expires: new Date(0),
-            path: '/',
-        }));
+  if (req.method === "POST") {
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("userSession", "", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // Can be altered when having env file, as of now it's gonna just handle out lower environment
+        expires: new Date(0),
+        path: "/",
+      })
+    );
 
-        return res.status(200).json({ message: 'Logged out successfully' });
-    }
+    return res.status(200).json({ message: "Logged out successfully" });
+  }
 
-    return res.status(405).json({ error: 'Method Not Allowed' });
+  return res.status(405).json({ error: "Method Not Allowed" });
 }
--- src/pages/api/set-session/index.ts
+++ src/pages/api/set-session/index.ts
@@ -1,29 +1,31 @@
-// pages/api/set-session.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-import cookie from 'cookie';
+import { NextApiRequest, NextApiResponse } from "next";
+import cookie from "cookie";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method === 'POST') {
-        const { username } = req.body;
+  if (req.method === "POST") {
+    const { username } = req.body;
 
-        if (!username) {
-            return res.status(400).json({ error: 'Username is required' });
-        }
-
-        // Set the cookie with username and expiration time
-        const expiry = new Date();
-        expiry.setSeconds(expiry.getSeconds() + 180); // Alter this value for adjusting session expiry
-
-        const cookieValue = JSON.stringify({ username });
-        res.setHeader('Set-Cookie', cookie.serialize('userSession', cookieValue, {
-            httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            expires: expiry,
-            path: '/',
-        }));
-
-        return res.status(200).json({ name: username });
+    if (!username) {
+      return res.status(400).json({ error: "Username is required" });
     }
 
-    return res.status(405).json({ error: 'Method Not Allowed' });
+    // Set the cookie with username and expiration time
+    const expiry = new Date();
+    expiry.setSeconds(expiry.getSeconds() + 180); // Alter this value for adjusting session expiry
+
+    const cookieValue = JSON.stringify({ username });
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("userSession", cookieValue, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // Can be altered when having env file, as of now it's gonna just handle out lower environment
+        expires: expiry,
+        path: "/",
+      })
+    );
+
+    return res.status(200).json({ name: username });
+  }
+
+  return res.status(405).json({ error: "Method Not Allowed" });
 }
